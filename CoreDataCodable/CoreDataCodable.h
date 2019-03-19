a2b793da9bc0ad72860330884b64b8c5fcdf6ccb
peterringset <peter@ringset.no>
2019-03-19 12:15:28 +0100

    Initial commit

---
+++ CoreDataCodable/CoreDataCodable.h
@@ -0,0 +1,19 @@
+//
+//  CoreDataCodable.h
+//  CoreDataCodable
+//
+//  Created by Peter Ringset on 19/03/2019.
+//  Copyright © 2019 Ringset. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CoreDataCodable.
+FOUNDATION_EXPORT double CoreDataCodableVersionNumber;
+
+//! Project version string for CoreDataCodable.
+FOUNDATION_EXPORT const unsigned char CoreDataCodableVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CoreDataCodable/PublicHeader.h>
+
+
---
+++ README.md
@@ -0,0 +1,2 @@
+# CoreDataCodable
+Protocols and extensions for making core data play nice with Swift's Codable protocols
